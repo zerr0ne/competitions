79d4b1c3f0004a21768d346ac9562f937909a8b6
GauthamN <gauthamnekk@gmail.com>
2017-09-22 17:12:39 +0200

    Hopeful Test

--- tests/competition.js
+++ tests/competition.js
@@ -1,23 +1,52 @@
 var Web3 = require('web3');
 var chai = require('chai');
-var fs = require("fs");
+var fs = require('fs');
 var assert = chai.assert;
+var abi = JSON.parse(fs.readFileSync('./out/Competition.abi', 'utf8'));
+var bin = fs.readFileSync('./out/Competition.bin', 'utf8');
+const TERMS_AND_CONDITIONS = '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad';
+const MELON_ASSET = '0x2a20ff70596e431ab26c2365acab1b988da8eccf';
 
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-var defaultAccount;
-web3.eth.getAccounts()
-.then(function(accounts) {
-  defaultAccount = accounts[0];
-})
+describe('Competition', function() {
 
-var file = JSON.parse(fs.readFileSync("./out/Final.sol.json", "utf8"));
-console.log(file);
-describe('Array', function() {
-  it('should start empty', function() {
-    return web3.eth.getGasPrice()
-    .then(function(n) {
-      console.log(n);
+  let defaultAccount;
+  let contract;
+  before(function(){
+    return web3.eth.getAccounts()
+    .then(function(accounts) {
+      defaultAccount = accounts[0];
+      contract = new web3.eth.Contract(abi);
+      return contract.deploy({
+          data: bin,
+          arguments: [MELON_ASSET]
+      }).send({from: defaultAccount, gas:1000000});
+    })
+    .then(function(result) {
+      contract = result;
+    })
+  })
+
+  it('Check if Registration leads to Hopeful Entry', function() {
+    let hash = web3.utils.soliditySha3('\x19Ethereum Signed Message:\n32', TERMS_AND_CONDITIONS);
+    return web3.eth.sign(hash, defaultAccount)
+    .then(function(sig) {
+      sig = sig.substr(2, sig.length);
+      let r = '0x' + sig.substr(0, 64);
+      let s = '0x' + sig.substr(64, 64);
+      let v = parseFloat(sig.substr(128, 2)) + 27;
+      //MELON_ASSET as Dummy for Fund Address
+      return contract.methods.registerForCompetition(
+        MELON_ASSET, MELON_ASSET, MELON_ASSET, 20, v, r, s
+      )
+      .call({from: defaultAccount});
+    })
+    .then(function(result) {
+      return contract.methods.hopefuls(0).call();
+    })
+    .then(function(hopeful) {
+      assert.equal(hopeful[1], defaultAccount);
     })
   });
 });
