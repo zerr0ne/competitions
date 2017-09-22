e5d5eaadcfa6cf6866d25263bdcd0d5852e3b925
GauthamN <gauthamnekk@gmail.com>
2017-09-22 17:17:50 +0200

    Added missing Files

---
+++ src/Competition.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.16;
+
+import "ds-test/test.sol";
+
+import "./Competition.sol";
+
+contract CompetitionTest is DSTest {
+    Competition competition;
+
+    function setUp() {
+        //competition = new Competition();
+    }
+
+    function testFail_basic_sanity() {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() {
+        assertTrue(true);
+    }
+}
