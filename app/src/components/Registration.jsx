f402f443520f2f2caa0f5f34daa04a92cc0b417d
GauthamN <gauthamnekk@gmail.com>
2017-09-25 20:45:45 +0200

    Fixed the signing error, Updated app and tests

--- app/src/components/Registration.jsx
+++ app/src/components/Registration.jsx
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import VaultJson from '../contracts/Vault.json';
 import competitionAbi from '../contracts/Competition.json';
 
-const competitionAddress = '0xE87F3F1c7081238e51E04cCF14cf10d567993DED';
+const competitionAddress = '0x4BF80bdB19C1Af50744FF4DbEe3a4029BC3D494D';
 const melonToken = '0x2a20ff70596e431ab26C2365acab1b988DA8eCCF';
 const TERMS_AND_CONDITIONS = '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad';
+const TERMS_AND_CONDITIONS_METAMASK =
+  '0x93100cc9477ba6522a2d7d5e83d0e075b167224ed8aa0c5860cfd47fa9f22797';
 
 class Registration extends Component {
   constructor(props) {
     super(props);
-    this.state = { fundName: '' };
+    this.state = { fundName: '', loading: 0 };
     this.handleNext = this.handleNext.bind(this);
     this.handleBack = this.handleBack.bind(this);
   }
@@ -22,13 +24,15 @@ class Registration extends Component {
   }
 
   async sign() {
-    const hash = await this.props.web3.utils.soliditySha3(
-      '\x19Ethereum Signed Message:\n32',
-      TERMS_AND_CONDITIONS,
-    );
+    console.log(this.props.web3.currentProvider.isMetaMask);
+    let hash = TERMS_AND_CONDITIONS;
+    if (this.props.web3.currentProvider.isMetaMask) {
+      hash = TERMS_AND_CONDITIONS_METAMASK;
+    }
     let sig = await this.props.web3.eth.sign(hash, this.props.account);
     sig = sig.substr(2, sig.length);
     const r = `0x${sig.substr(0, 64)}`;
+    console.log(r);
     const s = `0x${sig.substr(64, 64)}`;
     const v = parseFloat(sig.substr(128, 2)) + 27;
     return { r, s, v };
@@ -39,12 +43,13 @@ class Registration extends Component {
     try {
       const contract = await new this.props.web3.eth.Contract(competitionAbi, competitionAddress);
       const { r, s, v } = await this.sign();
-      // Currently works even when transactions fails with bad instruction
+      this.setState({ loading: 1 });
       await contract.methods
         .registerForCompetition(this.props.vaultAddress, melonToken, melonToken, 10, v, r, s)
         .send({ from: this.props.account });
       this.props.updateState('step', 3);
     } catch (err) {
+      console.log(err);
       this.props.updateState('errorMessage', 'Registration Failed. Try again');
     }
   }
@@ -56,6 +61,11 @@ class Registration extends Component {
   render() {
     return (
       <form onSubmit={this.handleNext}>
+        {this.state.loading === 1 && (
+          <div className="alert alert-info" role="alert">
+            <b>Please wait</b>, your registration is being processed
+          </div>
+        )}
         <h3>Fund Name : {this.state.fundName}</h3>
         <label htmlFor="terms">Terms and Conditions</label>
         <p>Some Placeholder</p>
