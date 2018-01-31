fe1933ab8966c76dc57a9b603707945d2a4b9aec
GauthamN <gauthamnekk@gmail.com>
2018-01-31 13:28:26 +0100

    Add error message for Metamask

--- app/src/index.js
+++ app/src/index.js
@@ -8,17 +8,27 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 window.addEventListener('load', async () => {
-  let web3Provided;
-  if (typeof web3 !== 'undefined') {
-    web3Provided = new Web3(web3.currentProvider);
-  } else {
-    web3Provided = new Web3(new Web3.providers.HttpProvider('https://localhost:8545'));
+  try {
+    let web3Provided;
+    if (typeof web3 !== 'undefined') {
+      web3Provided = new Web3(web3.currentProvider);
+    } else {
+      web3Provided = new Web3(new Web3.providers.HttpProvider('https://localhost:8545'));
+    }
+    const accounts = await web3Provided.eth.getAccounts();
+
+    ReactDOM.render(
+      <App web3={web3Provided} account={accounts[0]} />,
+      document.getElementById('root'),
+    );
   }
-
-  const accounts = await web3Provided.eth.getAccounts();
-
-  ReactDOM.render(
-    <App web3={web3Provided} account={accounts[0]} />,
-    document.getElementById('root'),
-  );
+  catch (e) {
+    ReactDOM.render(
+      <div className="home-error">
+       <h2>Please connect to Metamask main network</h2>
+       <p>To register in our competition, you need to use a main net account on Metamask, Please make sure you have Metamask installed and set on main network.</p>
+      </div>,
+      document.getElementById('root'),
+    );
+  }
 });
